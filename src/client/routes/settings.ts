4a0ed285af58ae583f40a1dcc063e9064a738770
nichoth <nichoth@nichoth.com>
2026-01-27 12:26:03 -0800

    wip

--- src/client/routes/settings.ts
+++ src/client/routes/settings.ts
@@ -1,6 +1,7 @@
 import { html } from 'htm/preact'
 import { type FunctionComponent } from 'preact'
 import { type AppState } from '../state'
+import './settings.css'
 
 export const SettingsRoute:FunctionComponent<{
     state:AppState
