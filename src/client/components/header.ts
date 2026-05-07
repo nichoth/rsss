c57ecb28c97b844ceb4cdf1e9a482e3cf8205e32
nichoth <nichoth@nichoth.com>
2026-05-06 22:34:10 -0700

    rm sponsor button

--- src/client/components/header.ts
+++ src/client/components/header.ts
@@ -116,19 +116,6 @@ export const Header:FunctionComponent<{
                 </a>
             </nav>
 
-            <div>
-                <iframe
-                    src="https://github.com/sponsors/nichoth/button"
-                    title="Sponsor nichoth"
-                    height="32"
-                    width="114"
-                    sandbox="allow-scripts allow-same-origin"
-                    loading="lazy"
-                    referrerpolicy="no-referrer"
-                    style="border: 0;"
-                ></iframe>
-            </div>
-
             <div class="mobile-user">
                 <${UserIcon} state=${state} />
                 ${user.value && html`
